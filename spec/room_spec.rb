8ccb0aa7cb6c449450e8c07aeaee83ed69d24d76
Gavin Carew <gjcarew@gmail.com>
2022-07-15 22:08:42 -0700

    Finish room_spec

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -20,5 +20,18 @@ RSpec.describe Room do
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
     end
+
+    xit 'can tell if painted' do
+      room1 = Room.new(:bedroom, 10, '13')
+      expect(room.is_painted?).to be false
+    end
+
+    xit 'can be painted' do
+      room1 = Room.new(:bedroom, 10, '13')
+      expect(room.is_painted?).to be false
+      room.paint
+      expect(room.is_painted?).to be false
+    end
+
   end
 end
